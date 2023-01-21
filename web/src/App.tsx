dea985a4154123ece9f00b4de46ed7a895665121
FranciscoVieir <franciscovieira6397@gmail.com>
2023-01-21 09:02:17 -0300

    feat: finalizing the project

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-// import './styles/global.css'
+import './lib/dayjs'
 import { Header } from './components/Header'
 import { SummaryTable } from './components/SummaryTable'
 import './styles/global.css'
--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,6 +1,7 @@
 import { Check } from "phosphor-react";
-import * as Checkbox from '@radix-ui/react-checkbox'
+import * as Checkbox from '@radix-ui/react-checkbox';
 import { FormEvent, useState } from "react";
+import { api } from "../lib/axios";
 
 const availableWeekDays = [
   'Domingo',
@@ -9,28 +10,38 @@ const availableWeekDays = [
   'Quarta-feira',
   'Quinta-feira',
   'Sexta-feira',
-  'Sábado'
+  'Sábado',
 ]
 
 export function NewHabitForm() {
+
   const [title, setTitle] = useState('')
   const [weekDays, setWeekDays] = useState<number[]>([])
 
-
-  function createNewHabit(event: FormEvent) {
+  async function createNewHabit(event: FormEvent) {
     event.preventDefault()
 
-    console.log(title, weekDays)
+    if (!title || weekDays.length === 0) {
+      return
+    }
+
+    await api.post('habits', {
+      title,
+      weekDays
+    })
+
+    setTitle('')
+    setWeekDays([])
+
+    alert('Hábito criado com sucesso!')
   }
 
   function handleToggleWeekDay(weekDay: number) {
     if (weekDays.includes(weekDay)) {
-      const weekDaysWithRemovedOne = weekDays.filter((day) => day !== weekDay)
-
+      const weekDaysWithRemovedOne = weekDays.filter(day => day !== weekDay)
       setWeekDays(weekDaysWithRemovedOne)
     } else {
       const weekDaysWithAddedOne = [...weekDays, weekDay]
-
       setWeekDays(weekDaysWithAddedOne)
     }
   }
@@ -38,49 +49,47 @@ export function NewHabitForm() {
   return (
     <form onSubmit={createNewHabit} className="w-full flex flex-col mt-6">
       <label htmlFor="title" className="font-semibold leading-tight">
-        Qual o seu comprometimento?
+        Qual seu comprometimento?
       </label>
 
       <input
         type="text"
         id="title"
-        placeholder="ex.: Exercícios, dormir bem, etc.."
-        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+        placeholder="ex.: Exercícios, dormir bem, etc..."
+        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
         autoFocus
-        onChange={(event => setTitle(event.target.value))}
+        value={title}
+        onChange={event => setTitle(event.target.value)}
       />
+
       <label htmlFor="" className="font-semibold leading-tight mt-4">
         Qual a recorrência?
       </label>
 
       <div className="flex flex-col gap-2 mt-3">
-        {availableWeekDays.map((weekDay, index) => {
-          return (
-            <Checkbox.Root
-              key={weekDay}
-              className="flex items-center gap-3 group"
-              onCheckedChange={() => {
-                handleToggleWeekDay(index)
-              }}
-            >
-              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
-                <Checkbox.Indicator>
-                  <Check size={20} className="text-white" />
-                </Checkbox.Indicator>
-              </div>
+        {availableWeekDays.map((weekDay, index) => (
+          <Checkbox.Root
+            key={weekDay}
+            className='flex items-center gap-3 group focus:outline-none'
+            checked={weekDays.includes(index)}
+            onCheckedChange={() => handleToggleWeekDay(index)}
+          >
+            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-50 transition-colors group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-background'>
+              <Checkbox.Indicator>
+                <Check size={20} className="text-white" />
+              </Checkbox.Indicator>
+            </div>
 
-              <span className='text-white leading-tight'>
-                {weekDay}
-              </span>
-            </Checkbox.Root>
-          )
-        })}
-
+            <span className='text-white leading-tight'>
+              {weekDay}
+            </span>
+          </Checkbox.Root>
+        ))}
       </div>
 
       <button
-        className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500"
         type="submit"
+        className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500 transition-colors focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
       >
         <Check size={20} weight="bold" />
         Confirmar
